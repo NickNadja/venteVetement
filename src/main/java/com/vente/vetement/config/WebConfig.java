87a7fa1fa9620f5f4745b350f189ec57096ea6b6
NickNadja <nadjanick3@gmail.com>
2025-03-23 21:08:31 +0100

    avec webconfig non commente

--- src/main/java/com/vente/vetement/config/WebConfig.java
+++ src/main/java/com/vente/vetement/config/WebConfig.java
@@ -1,17 +1,17 @@
-// package com.vente.vetement.config;
+package com.vente.vetement.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// import java.nio.file.Paths;
+import java.nio.file.Paths;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
-//     @Override
-//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//         String uploadDir = Paths.get("uploads").toAbsolutePath().toString();
-//         registry.addResourceHandler("/uploads/**")
-//                 .addResourceLocations("file:" + uploadDir + "/");
-//     }
-// }
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadDir = Paths.get("uploads").toAbsolutePath().toString();
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadDir + "/");
+    }
+}
